441a4aab6cfe4c06ad3d2e18c6fb6356da1d4cf8
guilhermepassos1 <gepassos06@gmail.com>
2023-06-05 16:08:05 -0300

    atualização

--- src/App.vue
+++ src/App.vue
@@ -15,41 +15,7 @@ function formatarPreco(preco) {
     <div class="listagem-livros">
      <Cardlivro v-for="livro in livros" :key="livro.id" :livro="livro" @adicionarAoCarrinho="adicionarAoCarrinho" />
     </div>
-    <div class="carrinho">
-      <h2>Meu carrinho</h2>
-      <div class="wrap-carrinho">
-        <p v-if="carrinho.itens.length === 0">Seu carrinho está vazio</p>
-        <div v-else>
-          <div class="item-carrinho" v-for="(item, index) in carrinho.itens" :key="index">
-            <div class="info-livro">
-              <div class="imagem-livro">
-                <img :src="item.img" class="icon-capa-livro" />
-              </div>
-              <div class="detalhes-livro">
-                <div>
-                  <p>{{ item.title }}</p>
-                  <p class="info-livro-preco">{{ formatarPreco(item.price) }}/un</p>
-                </div>
-                <div>
-                  <p>
-                    Quantidade:
-                    <input
-                      type="number"
-                      v-model="item.quantidade"
-                      @change="atualizaQuantidadeItem(item)"
-                      min="1"
-                    />
-                  </p>
-                  <button @click="removerItemCarrinho(item)">&#128465;</button>
-                  <p>Total: {{ formatarPreco(item.total) }}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <p class="carrinho-total">Total: {{ formatarPreco(carrinho.total) }}</p>
-      </div>
-    </div>
+   
   </div>
 </template>
 
@@ -61,7 +27,7 @@ function formatarPreco(preco) {
   font-weight: bold;
 }
 
-.item-carrinho .info-livro {
+.item-carrinho .info-livro { 
   display: flex;
   margin-bottom: 10px;
 }
